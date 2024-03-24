c06a8fe7f70b31ae640e138a02200e51d765d23a
Ravi N <your.email@example.com>
2024-03-24 21:56:59 +0530

    Swagger starter set up

---
+++ swagger-course/src/main/java/com/ravi/swaggercourse/SwaggerCourseApplication.java
@@ -0,0 +1,13 @@
+package com.ravi.swaggercourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwaggerCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwaggerCourseApplication.class, args);
+	}
+
+}
---
+++ swagger-course/src/test/java/com/ravi/swaggercourse/SwaggerCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ravi.swaggercourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwaggerCourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
